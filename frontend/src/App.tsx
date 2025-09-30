386eee22c82795aa6a07bfeb471dd833b8374465
DIER10 <diego.beltran@uptc.edu.co>
2025-09-29 23:39:21 -0500

    Solición de errores de build

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/login/LoginPage";
 import AdminLayout from "./components/admin/AdminLayout";
