1b687af530a68d20fb8e10e2fb6c7e019372835c
Brayan-Cifuentes17 <brayan.cifuentes@uptc.edu.co>
2025-10-05 11:40:38 -0500

    Subiendo cambios sin carpeta backend

--- backend/jest.config.cjs
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  testEnvironment: 'node',
-  transform: {
-    '^.+\\.js$': 'babel-jest',
-  },
-  setupFilesAfterEnv: ['<rootDir>/src/__tests__/jest.setup.js'],
-};
--- backend/src/services/resourceService.js
+++
@@ -1,7 +0,0 @@
-import { db } from "../config/firebase.js";
-
-export async function getResourceDoc(id, collection) {
-    const resourceDoc = await db.collection(collection).doc(id).get();
-
-    return resourceDoc.data() || null;
-}
