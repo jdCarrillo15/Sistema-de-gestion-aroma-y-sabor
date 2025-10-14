0c38ede51547de7098dcef1bc114ac437449cb05
DIER10 <diego.beltran@uptc.edu.co>
2025-10-13 19:46:01 -0500

    Correción de rutas

--- frontend/src/services/sockets/socket.ts
+++ frontend/src/services/sockets/socket.ts
@@ -2,8 +2,8 @@ import { io, Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
-const SOCKET_URL = "http://localhost:3000";
-//const SOCKET_URL = "http://185.144.159.8:3000";
+//const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = "http://148.230.93.151/:3000";
 
 declare global {
   interface Window {
